ccec5f9cf62b6154fd06fbb5588b677b5d2e6baa
ZyeLangX <131007575+zyelangx@users.noreply.github.com>
2023-04-25 21:26:25 +0700

    Example

---
+++ app.js
@@ -0,0 +1,33 @@
+__path = process.cwd()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+// cors agar support web Xixi:)
+app.use(cors())
+
+// Api fitur
+app.get('/api/:path/:file', (req, res) => {
+    const path = req.params.path
+    const file = req.params.file
+    try {
+        require(__path + `/api/${path}/${file}.js`)(req, res)
+    } catch (err) {
+        return res.status(404).sendFile(__path + '/views/404.html')
+    }
+})
+
+// Buat Sendiri Home Pagenya
+app.get('/', (req, res) => {
+    res.sendFile(__path + '/views/index.html')
+})
+// Not Found
+app.all('*', (req, res) => {
+    res.status(404).sendFile(__path + '/views/404.html')
+});
+
+app.listen('8000', () => {
+    console.log('Server Running Port ' + 8000)
+})
+
+module.exports = app
